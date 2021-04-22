427d7e8fddbf7e40c2923d3fe042cd560e3724ab
Polina Ponomareva <pp09117@gmail.com>
2021-04-22 17:57:27 +0300

    feat: настроить конфигурацию проекта(настроить линтер и тд) + подключиться к mongo

---
+++ app.js
@@ -0,0 +1,17 @@
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
