4091a1fa40888f4a46403e600f4e3d455bebbf59
Polina Ponomareva <pp09117@gmail.com>
2021-04-23 16:01:01 +0300

    feat: добавить обработку ошибок в контроллерах для пути /users

--- controllers/users.js
+++ controllers/users.js
@@ -8,15 +8,27 @@ const getUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err));
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 module.exports = { getUsers, getUserById, createUser };
