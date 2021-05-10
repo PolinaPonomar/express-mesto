899c19f13b33f0ae748363a612aeca71ae97ddd1
Polina Ponomareva <pp09117@gmail.com>
2021-05-10 03:03:37 +0300

    feat: добавить комментарии

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,8 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const JWT_SECRET = '071d5a0c28a4e7b50bb9712284f25c7fcf31c62680c024094011c56d70586c4e';
 
 module.exports = (req, res, next) => {
-  if (!req.cookies.jwt) { // поверяем, есть в заголовке cookie jwt токен
+  // eslint-disable-next-line max-len
+  if (!req.cookies.jwt) { // поверяем, есть ли в заголовке cookie jwt-токен (тут уже распарсенное значение)
     const err = new UnauthorizedError('Необходима авторизация');
     next(err);
   }
